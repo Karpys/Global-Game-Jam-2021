e206b7cae0e8633445f426e6e7ece874b58ac2d0
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-01-30 14:14:03 +0100

    [Feat] Spawner Ennemy and Pat

---
+++ Assets/Ennemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ListTransform TransformManager;
+    public Vector2 PosToGo;
+    public float speed;
+    public int id;
+    public List<int> ListId;
+    void Start()
+    {
+        /*PosToGo = ListPat[index].transform.position;*/
+
+        PosToGo = TransformManager.ListTrans[ListId[id]].transform.position;
+        transform.position = PosToGo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PosToGo,speed);
+        Vector2 PosActor = new Vector2(transform.position.x, transform.position.y);
+
+        if(PosActor==PosToGo)
+        {
+            FindNextPat();
+        }
+
+    }
+
+    public void FindNextPat()
+    {
+        id += 1;
+        if(ListId.Count==id)
+        {
+            id = 2;
+        }
+
+        PosToGo = TransformManager.ListTrans[ListId[id]].transform.position;
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Ennemy;
+    public GameObject Player;
+    public ListTransform Trans;
+    /*public List<int> ListIdProvi;*/
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject En = Instantiate(Ennemy, transform.position, transform.rotation);
+            En.GetComponent<Ennemy>().TransformManager = Trans;
+            int rdm = Random.Range(1, 5);
+            List<int> ListProvi = new List<int>();
+            if(rdm==1)
+            {
+                ListProvi.Add(24);
+                ListProvi.Add(0);
+            }else if(rdm==2)
+            {
+                ListProvi.Add(25);
+                ListProvi.Add(7);
+            }
+            else if (rdm == 3)
+            {
+                ListProvi.Add(26);
+                ListProvi.Add(16);
+            }
+            else if (rdm == 4)
+            {
+                ListProvi.Add(27);
+                ListProvi.Add(23);
+            }
+
+            rdm = Random.Range(1, 5);
+
+            if (rdm == 1)
+            {
+                ListProvi.AddRange(new List<int>() { 0, 7, 23, 16 });
+                Debug.Log("Carrée");
+            }
+            else if (rdm == 2)
+            {
+                ListProvi.AddRange(new List<int>() { 16, 0, 7, 0});
+            }
+            else if (rdm == 3)
+            {
+                ListProvi.AddRange(new List<int>() {7,23,16,23});
+            }
+            else if (rdm == 4)
+            {
+                ListProvi.AddRange(new List<int>() {8,4,15,20});
+            }
+            En.GetComponent<Ennemy>().ListId = ListProvi;
+        }
+    }
+}
